d9ae6c8c3fae8a7a9e91dfc7a8a943b73d0b93b6
Driesboy <coenendries3@gmail.com>
2016-05-15 15:13:24 +0200

    Create test.php

---
+++ testplugin/src/MCrafterss/test.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MCrafterss;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use pocketmine\event\player\PlayerJoinEvent;
+
+use Mcrafters\MultiLanguage\Main;
+
+
+class test extends PluginBase implements Listener {
+
+	public function onEnable(){
+        	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+   	}
+
+	public function onJoin(PlayerJoinEvent $event){
+		$player = $event->getPlayer();
+        	$player->sendMessage(MultiLanguage::getInstance()->translate($player, "testmessage"));
+        }
+}
